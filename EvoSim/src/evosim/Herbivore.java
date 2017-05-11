c99ba5a4f8e46acf61fa9c11cb2214386b108a4d
bryanmcguffin <bryanmcguffin@pcp133883pcs.wireless.calpoly.edu>
2017-05-11 12:31:32 -0700

    Redesigned constructor and reproduce() method to correctly create a child object.

--- EvoSim/src/evosim/Herbivore.java
+++ EvoSim/src/evosim/Herbivore.java
@@ -34,9 +34,9 @@ public class Herbivore extends Creature implements Herbivorous
      * @param belly the amount of food the herbivore can store.
      * @param lifespan the amount of turns the creature can exist.
      */
-    public Herbivore(int health, int attack, int defense, int speed, int size, int belly, int lifespan)
+    public Herbivore(int health, int attack, int defense, int speed, double gRate, int belly, int lifespan)
     {
-        super(health, attack, defense, speed, size, belly, lifespan);
+        super(health, attack, defense, speed, gRate, belly, lifespan);
     }
     
     /**Creates a new Herbivore offspring from two parents, this and the other.
@@ -46,13 +46,26 @@ public class Herbivore extends Creature implements Herbivorous
      */
     public Herbivore reproduce(Herbivore other)
     {
-        return (Herbivore) super.reproduce(other);
+         if (other instanceof Herbivore)
+        {
+            Herbivore ho = (Herbivore) other;
+            int newHP = (this.hp + ho.getHP()) / 2;
+            int newAtt = (this.at + ho.getAttack()) / 2;
+            int newDef = (this.de + ho.getDefense()) / 2;
+            int newSpd = (this.sp + ho.getSpeed()) / 2;
+            double newGr = (this.growthRate + ho.getGrowthRate()) / 2;
+            int newBl = (this.belly + ho.getBelly()) / 2;
+            int newLife = (this.lifetime + ho.getLifetime()) / 2;
+            return new Herbivore(newHP, newAtt, newDef, newSpd, newGr, newBl, newLife);
+        }
+        return null;
     }
 
     /**While the creature is still hungry and the plant can still be eaten,
      * take another bite.
      * 
      * @param plant 
+     * @pre plant.isAlive() && this.isHungry()
      */
     @Override
     public void eat(Plant plant)
