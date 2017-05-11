1dc0f45ab02f0696184571ff7290511b959789b6
bryanmcguffin <bryanmcguffin@10.151.121.88>
2017-05-11 16:03:33 -0700

    Created Map class to keep track of existing creatures and their whereabouts in a 2D grid.

---
+++ EvoSim/src/evosim/Map.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evosim;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ *
+ * @author bryanmcguffin
+ */
+public class Map
+{
+
+    public Object[][] grid;
+
+    Vector<Organism> life;
+    Vector<Integer> xValue;
+    Vector<Integer> yValue;
+
+    public Map()
+    {
+        life = new Vector<Organism>();
+        xValue = new Vector<Integer>();
+        yValue = new Vector<Integer>();
+        grid = new Object[EvoConstants.MAP_SIZE][EvoConstants.MAP_SIZE];
+        for (int i = 0; i < EvoConstants.MAP_SIZE; i++)
+        {
+            for (int j = 0; j < EvoConstants.MAP_SIZE; j++)
+            {
+                grid[i][j] = null;
+            }
+        }
+    }
+
+    private int getIndex(Organism o)
+    {
+        return life.indexOf(o);
+    }
+
+    public void updatePosition(Organism o, int x, int y)
+    {
+        
+        int index = getIndex(o);
+        xValue.setElementAt(x, index);
+        yValue.setElementAt(y, index);
+    }
+
+    public int getXPosition(Organism o)
+    {
+        int index = getIndex(o);
+        return xValue.elementAt(index);
+    }
+
+    public int getYPosition(Organism o)
+    {
+        int index = getIndex(o);
+        return yValue.elementAt(index);
+    }
+
+    public boolean addOrganismToTable(Organism o, int x, int y)
+    {
+        if (life.size() < (EvoConstants.MAP_SIZE * EvoConstants.MAP_SIZE))
+        {
+            life.add(o);
+            xValue.add(x);
+            yValue.add(y);
+            grid[x][y] = o;
+            return o.setPosition(x, y);
+        }
+        return false;
+    }
+
+    public void removeOrganismFromTable(Organism o)
+    {
+        int index = life.indexOf(o);
+        int x = xValue.elementAt(index);
+        int y = yValue.elementAt(index);
+        life.removeElementAt(index);
+        xValue.removeElementAt(index);
+        yValue.removeElementAt(index);
+        grid[x][y] = null;
+    }
+
+}
