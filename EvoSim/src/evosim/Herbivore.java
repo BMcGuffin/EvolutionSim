b60b8ea4f67cd8e7b20c55be18c0900181bbc39b
bryanmcguffin <bryanmcguffin@10.151.121.88>
2017-05-10 22:43:45 -0700

    Fleshed out the class. Added a couple of constructors.

--- EvoSim/src/evosim/Herbivore.java
+++ EvoSim/src/evosim/Herbivore.java
@@ -16,21 +16,51 @@ package evosim;
 public class Herbivore extends Creature implements Herbivorous
 {
 
+    /**Generates a new herbivore from scratch.
+     * 
+     */
+    public Herbivore()
+    {
+        super();
+    }
+    
+    /**Generates a new herbivore from existing parameters.
+     * 
+     * @param health
+     * @param attack
+     * @param defense
+     * @param speed
+     * @param size
+     * @param belly the amount of food the herbivore can store.
+     * @param lifespan the amount of turns the creature can exist.
+     */
     public Herbivore(int health, int attack, int defense, int speed, int size, int belly, int lifespan)
     {
         super(health, attack, defense, speed, size, belly, lifespan);
     }
     
-    
+    /**Creates a new Herbivore offspring from two parents, this and the other.
+     * 
+     * @param other the herbivore to be "mated" with this one
+     * @return a new herbivore with a mix of its parents' traits
+     */
     public Herbivore reproduce(Herbivore other)
     {
         return (Herbivore) super.reproduce(other);
     }
 
+    /**While the creature is still hungry and the plant can still be eaten,
+     * take another bite.
+     * 
+     * @param plant 
+     */
     @Override
     public void eat(Plant plant)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        plant.damage(1);
+        this.fullness++;
+        if(plant.isAlive() && this.isHungry())
+            eat(plant);
     }
     
 }
