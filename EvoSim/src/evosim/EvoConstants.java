b65e10c6ccef6bc95532a5277c0a7d4b4c61bb8d
bryanmcguffin <bryanmcguffin@pcp133883pcs.wireless.calpoly.edu>
2017-05-11 12:32:00 -0700

    Changed size to a double. Increased some level caps.

--- EvoSim/src/evosim/EvoConstants.java
+++ EvoSim/src/evosim/EvoConstants.java
@@ -17,7 +17,7 @@ public final class EvoConstants
     public static final int INIT_ATTACK = 5;
     public static final int INIT_DEFENSE = 5;
     public static final int INIT_SPEED = 5;
-    public static final int INIT_SIZE = 5;
+    public static final double INIT_SIZE = 1.0;
     public static final int INIT_BELLY = 5;
     public static final int INIT_LIFESPAN = 5;
     public static final double INIT_GROWTH_RATE = 0.05;
@@ -27,9 +27,9 @@ public final class EvoConstants
     public static final int CAP_ATTACK = 100;
     public static final int CAP_DEFENSE = 100;
     public static final int CAP_SPEED = 100;
-    public static final int CAP_CREATURE_SIZE = 100;
+    public static final double CAP_CREATURE_SIZE = 1000;
     public static final int CAP_BELLY = 500;
-    public static final int CAP_LIFESPAN = 500;
-    public static final int CAP_PLANT_SIZE = 2000;
+    public static final int CAP_LIFESPAN = 200;
+    public static final double CAP_PLANT_SIZE = 5000;
     public static final double CAP_GROWTH_RATE = 0.5;
 }
