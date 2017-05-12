587cbed6f8c354c6c02fd465e0b674f968de744f
bmcguffi <bmcguffi@csc301x31.ad.calpoly.edu>
2017-05-12 15:00:34 -0700

    Broke up major functions into two methods: one which adds a random number of random organisms, and one which iterates all entities through one "turn." Both can surely be further broken down.

--- EvoSim/src/evosim/Main.java
+++ EvoSim/src/evosim/Main.java
@@ -18,6 +18,62 @@ import java.util.logging.Logger;
 public class Main
 {
 
+    public static void addRandomOrganisms(Map map)
+    {
+        final int typesOfOrganisms = 3;
+        Random rand = new Random();
+        final int numberToAdd = rand.nextInt(EvoConstants.MAP_MAXIMUM_ENTITIES - 1) + 1;
+        boolean placed = false;
+        for (int i = 0; i < numberToAdd; i++)
+        {
+            int type = rand.nextInt(typesOfOrganisms);
+            Organism o = null;
+            switch (type)
+            {
+                case 0:
+                    o = new Plant();
+                    break;
+                case 1:
+                    o = new Carnivore();
+                    break;
+                case 2:
+                    o = new Herbivore();
+                    break;
+            }
+            while (!placed)
+            {
+                int newX = rand.nextInt(EvoConstants.MAP_SIZE);
+                int newY = rand.nextInt(EvoConstants.MAP_SIZE);
+                placed = map.addOrganismToTable(o, newX, newY);
+            }
+            placed = false;
+        }
+    }
+
+    public static void takeTurn(Map map)
+    {
+        for (int i = 0; i < map.numberOfOrganisms(); i++)
+        {
+            Random r = new Random();
+            ((Organism) (map.getOrganism(i))).grow();
+            if (!((Organism) (map.getOrganism(i))).isAlive())
+            {
+                map.removeOrganismFromTable(map.getOrganism(i));
+                i--;
+            } else
+            {
+                if (map.getOrganism(i) instanceof Mobile)
+                {
+                    int moveDist = ((Mobile) ((Organism) (map.getOrganism(i)))).getSpeed();
+                    int newX = r.nextInt(moveDist) * (int) Math.pow(-1, r.nextInt(2));
+                    int newY = r.nextInt(moveDist) * (int) Math.pow(-1, r.nextInt(2));
+                    ((Mobile) ((Organism) (map.getOrganism(i)))).move(newX, newY, map);
+                }
+            }
+
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -35,63 +91,22 @@ public class Main
                 mpd.setVisible(true);
             }
         });
-        System.out.println("what");
         try
         {
             sleep(1000);
-        }
-        catch (InterruptedException ex)
+        } catch (InterruptedException ex)
         {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        map.addOrganismToTable(new Carnivore(), 4, 5);
-        map.addOrganismToTable(new Carnivore(), 1, 1);
-        map.addOrganismToTable(new Carnivore(), 8, 3);
-        map.addOrganismToTable(new Carnivore(), 6, 4);
-        map.addOrganismToTable(new Carnivore(), 9, 2);
-        map.addOrganismToTable(new Carnivore(), 2, 6);
-        map.addOrganismToTable(new Carnivore(), 5, 8);
-        map.addOrganismToTable(new Carnivore(), 3, 9);
-
-        map.addOrganismToTable(new Herbivore(), 18, 9);
-        map.addOrganismToTable(new Herbivore(), 8, 19);
-        map.addOrganismToTable(new Herbivore(), 18, 19);
-        map.addOrganismToTable(new Herbivore(), 8, 9);
-        map.addOrganismToTable(new Herbivore(), 6, 6);
-        try
-        {
-            sleep(1000);
-        }
-        catch (InterruptedException ex)
-        {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        map.addOrganismToTable(new Plant(), 7, 7);
-        map.addOrganismToTable(new Plant(), 1, 2);
-        map.addOrganismToTable(new Plant(), 4, 4);
-        map.addOrganismToTable(new Plant(), 17, 17);
+        addRandomOrganisms(map);
 
         while (true)
         {
-            for (int i = 0; i < map.numberOfOrganisms(); i++)
-            {
-                Random r = new Random();
-
-                if (map.getOrganism(i) instanceof Mobile)
-                {
-                    int moveDist = ((Mobile) ((Organism) (map.getOrganism(i)))).getSpeed();
-                    int newX = r.nextInt(moveDist) * (int) Math.pow(-1, r.nextInt(2));
-                    int newY = r.nextInt(moveDist) * (int) Math.pow(-1, r.nextInt(2));
-                    ((Mobile) ((Organism) (map.getOrganism(i)))).move(newX, newY, map);
-
-                }
-
-            }
+            takeTurn(map);
             try
             {
-                sleep(800);
-            }
-            catch (InterruptedException ex)
+                sleep(500);
+            } catch (InterruptedException ex)
             {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
