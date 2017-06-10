6bce8164731d4a0330f68204a69199f9b0fe5b42
BMcGuffin <bryanmcguffin@gmail.com>
2017-06-10 11:26:29 -0700

    Constants are now assigned based on values in a static properties file.

--- EvoSim/src/evosimApp/EvoConstants.java
+++ EvoSim/src/evosimApp/EvoConstants.java
@@ -6,6 +6,7 @@
 package evosimApp;
 
 import evosimSources.Map;
+import java.util.Properties;
 
 /**
  * This class holds constants for other classes. These values should be
@@ -20,56 +21,92 @@ public final class EvoConstants
 {
 
     //Initializer values for creatures
-    public static final int INIT_HEALTH = 5;
-    public static final int INIT_ATTACK = 5;
-    public static final int INIT_DEFENSE = 5;
-    public static final int INIT_SPEED = 5;
-    public static final double INIT_SIZE = 1.0;
-    public static final int INIT_BELLY = 5;
-    public static final int INIT_LIFESPAN = 5;
-    public static final double INIT_GROWTH_RATE = 0.05;
+    public static int INIT_HEALTH;
+    public static int INIT_ATTACK;
+    public static int INIT_DEFENSE;
+    public static int INIT_SPEED;
+    public static double INIT_SIZE;
+    public static int INIT_BELLY;
+    public static int INIT_LIFESPAN;
+    public static double INIT_GROWTH_RATE;
 
     //Level caps
-    public static final int CAP_HEALTH = 1000;
-    public static final int CAP_ATTACK = 100;
-    public static final int CAP_DEFENSE = 100;
-    public static final int CAP_SPEED = 100;
-    public static final double CAP_CREATURE_SIZE = 1000;
-    public static final int CAP_BELLY = 500;
-    public static final int CAP_LIFESPAN = 200;
-    public static final double CAP_PLANT_SIZE = 5000;
-    public static final double CAP_GROWTH_RATE = 0.5;
+    public static int CAP_HEALTH;
+    public static int CAP_ATTACK;
+    public static int CAP_DEFENSE;
+    public static int CAP_SPEED;
+    public static double CAP_CREATURE_SIZE;
+    public static int CAP_BELLY;
+    public static int CAP_LIFESPAN;
+    public static double CAP_PLANT_SIZE;
+    public static double CAP_GROWTH_RATE;
 
     //Map stuff
     //The map object itself, assigned in main() at runtime
     public static Map MAP;
-    
-    public static final int MAP_SIZE = 20;
-    public static final int MAP_MAXIMUM_START_ENTITIES = (int)(MAP_SIZE * MAP_SIZE * 0.1);
-    public static final int MAP_MAXIMUM_FINAL_ENTITIES = (int)(MAP_SIZE * MAP_SIZE * 0.9);
+
+    public static int MAP_SIZE;
+    public static int MAP_MAXIMUM_START_ENTITIES;
+    public static int MAP_MAXIMUM_FINAL_ENTITIES;
 
     //The global unique ID number. Each organism has one; upon assignment, this
     //is incremented.
     public static int ID = 1;
-    
+
     //Debug
     //Whether or not to display debug statements, assigned true or false based on
     //command line arguments
     public static boolean debug;
     
-    /**Displays debug statements in code only if user has opted to view them at
+    public static boolean FOREVER;
+
+    /**
+     * Displays debug statements in code only if user has opted to view them at
      * runtime.
-     * 
+     *
      * @param str the string message to print to standard out
      */
     public static void debug(String str)
     {
-        if(debug)
+        if (debug)
         {
             System.out.println(str);
         }
     }
-    
-    public static final int turnDelay = 1000;
-    
+
+    /**
+     * Load global constants from properties file.
+     *
+     * @param p the properties object which holds data from the properties file
+     */
+    public static void loadConstants(Properties p)
+    {
+        TURN_DELAY = Integer.parseInt(p.getProperty("turn_delay_in_ms"));
+        
+        MAP_SIZE = Integer.parseInt(p.getProperty("map_size"));
+        MAP_MAXIMUM_START_ENTITIES = (int) (MAP_SIZE * MAP_SIZE * 0.1);
+        MAP_MAXIMUM_FINAL_ENTITIES = (int) (MAP_SIZE * MAP_SIZE * 0.9);
+        
+        INIT_HEALTH = Integer.parseInt(p.getProperty("init_health"));
+        INIT_ATTACK = Integer.parseInt(p.getProperty("init_attack"));
+        INIT_DEFENSE = Integer.parseInt(p.getProperty("init_defense"));
+        INIT_SPEED = Integer.parseInt(p.getProperty("init_speed"));
+        INIT_SIZE = Double.parseDouble(p.getProperty("init_size"));
+        INIT_BELLY = Integer.parseInt(p.getProperty("init_belly_size"));
+        INIT_LIFESPAN = Integer.parseInt(p.getProperty("init_lifespan"));
+        INIT_GROWTH_RATE = Double.parseDouble(p.getProperty("init_growth_rate"));
+        
+        CAP_HEALTH = Integer.parseInt(p.getProperty("cap_health"));
+        CAP_ATTACK = Integer.parseInt(p.getProperty("cap_attack"));
+        CAP_DEFENSE = Integer.parseInt(p.getProperty("cap_defense"));
+        CAP_SPEED = Integer.parseInt(p.getProperty("cap_speed"));
+        CAP_CREATURE_SIZE = Double.parseDouble(p.getProperty("cap_size"));
+        CAP_PLANT_SIZE = Double.parseDouble(p.getProperty("cap_plant_size"));
+        CAP_BELLY = Integer.parseInt(p.getProperty("cap_belly_size"));
+        CAP_LIFESPAN = Integer.parseInt(p.getProperty("cap_lifespan"));
+        CAP_GROWTH_RATE = Double.parseDouble(p.getProperty("cap_growth_rate"));
+        
+        FOREVER = (Integer.parseInt(p.getProperty("forever"))) != 0;
+    }
+    public static int TURN_DELAY;
 }
